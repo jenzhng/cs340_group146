0d4dabc986eecae346bd124595991b342476665b
arbiosu <amea@oregonstate.edu>
2024-02-20 22:11:59 -0500

    updated dummy data

--- DDL.sql
+++ DDL.sql
@@ -14,9 +14,10 @@ CREATE OR REPLACE TABLE Customers (
     firstName varchar(255) NOT NULL,
     lastName varchar(255) NOT NULL,
     email varchar(255) NOT NULL,
-    phoneNumber varchar(10) NOT NULL,
+    phoneNumber varchar(12) NOT NULL,
     PRIMARY KEY (customerID)
 );
+-- Record phoneNumber as 123-456-7890
 
 -- Create Orders Table
 CREATE OR REPLACE TABLE Orders (
@@ -26,6 +27,7 @@ CREATE OR REPLACE TABLE Orders (
     FOREIGN KEY (customerID) REFERENCES Customers (customerID),
     PRIMARY KEY (orderID)
 );
+-- Records when a Customer places an Order
 
 -- Create Records Table
 CREATE OR REPLACE TABLE Records (
@@ -54,6 +56,8 @@ CREATE OR REPLACE TABLE RecordOrders (
     FOREIGN KEY (recordID) REFERENCES Records (recordID),
     PRIMARY KEY (orderID, recordID)
 );
+-- Records what Record a Customer purchased and how many
+-- An Order can have multiple Records associated with it
 
 -- Create GenreRecords Table
 CREATE OR REPLACE TABLE GenreRecords (
@@ -63,44 +67,35 @@ CREATE OR REPLACE TABLE GenreRecords (
     FOREIGN KEY (genreID) REFERENCES Genres (genreID),
     PRIMARY KEY (recordID, genreID)
 );
-
+-- a Record can have multiple Genres and a Genre can have many Records associated with it
 
 -- Insert sample data
-INSERT INTO Customers(name, email)
-VALUES(
-	"Jane Smith",
-	"jsmith@gmail.com"
-),
-(
-	"Michael Jones",
-	"mjones@gmail.com"
-),
-(
-	"Corey Rodriguez",
-	"crodriguez@gmail.com"
-);
+INSERT INTO Customers(firstName, lastName, email, phoneNumber)
+VALUES("Jane", "Smith", "jsmith@gmail.com", "856-555-2230"),
+("Michael", "Jones", "mjones@gmail.com", "609-555-2251"),
+("Corey", "Rodriguez", "crodriguez@gmail.com", "215-555-7326");
 
-INSERT INTO Orders (cid, date)
-VALUES ((SELECT customerID FROM Customers WHERE name = 'Jane Smith'), '20240208'),
-((SELECT customerID FROM Customers WHERE name = 'Corey Rodriguez'), '20240206'),
-((SELECT customerID FROM Customers WHERE name = 'Michael Jones'), '20240207');
+INSERT INTO Orders (customerID, orderDate)
+VALUES ((SELECT customerID FROM Customers WHERE email = 'jsmith@gmail.com'), '20240208'),
+((SELECT customerID FROM Customers WHERE email = 'crodriguez@gmail.com'), '20240206'),
+((SELECT customerID FROM Customers WHERE email = 'mjones@gmail.com'), '20240207');
 
-INSERT INTO Records (title, artist, quantity, price)
-VALUES('Be the Cowboy', 'Mitski', 3, 30.00),
-('Rumors', 'Fleetwod Mac', 5, 50.00),
-("Over The Garden Wall Original Soundtrack LP", "Sam Wolfe Connelly", 1, 100.00);
+INSERT INTO Records (title, artist, qtyStock, price)
+VALUES('Be the Cowboy', 'Mitski', 100, 30.00),
+('Rumors', 'Fleetwod Mac', 100, 50.00),
+("Over The Garden Wall Original Soundtrack LP", "Sam Wolfe Connelly", 25, 100.00);
 
 INSERT INTO Genres (name, description)
 VALUES ('Indie', 'Alternative Music'), ('Soundtrack', 'Original Soundtrack for TV/Film'),
 ('Classic Rock', 'Rock music from the 1960s - 1990s');
 
-INSERT INTO RecordOrders (orderID, recordID)
-VALUES ((SELECT orderID from Orders WHERE cid = (SELECT customerID FROM Customers WHERE email = 'crodriguez@gmail.com')), 
-(SELECT recordID FROM Records WHERE title = 'Be the Cowboy')), 
-((SELECT orderID FROM Orders WHERE cid = (SELECT customerID FROM Customers WHERE email = 'mjones@gmail.com')), 
-(SELECT recordID FROM Records WHERE title = 'Over The Garden Wall Original Soundtrack LP')),
-((SELECT orderID FROM Orders WHERE cid = (SELECT customerID FROM Customers WHERE email = 'jsmith@gmail.com')),
-(SELECT recordID FROM Records WHERE title = 'Rumors'));
+INSERT INTO RecordOrders (orderID, recordID, qtyOrdered)
+VALUES ((SELECT orderID from Orders WHERE customerID = (SELECT customerID FROM Customers WHERE email = 'crodriguez@gmail.com')), 
+(SELECT recordID FROM Records WHERE title = 'Be the Cowboy'), 1), 
+((SELECT orderID FROM Orders WHERE customerID = (SELECT customerID FROM Customers WHERE email = 'mjones@gmail.com')), 
+(SELECT recordID FROM Records WHERE title = 'Over The Garden Wall Original Soundtrack LP'), 1),
+((SELECT orderID FROM Orders WHERE customerID = (SELECT customerID FROM Customers WHERE email = 'jsmith@gmail.com')),
+(SELECT recordID FROM Records WHERE title = 'Rumors'), 1);
 
 INSERT INTO GenreRecords (recordID, genreID)
 VALUES ((SELECT recordID FROM Records WHERE title = 'Be the Cowboy'), (SELECT genreID FROM Genres WHERE name = 'Indie')),
